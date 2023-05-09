5fb19ed7daf1a6c823f6ff03b955e96d72162ae6
Zachary <108593553+Zachary-Woo@users.noreply.github.com>
2023-05-09 14:23:17 -0400

    Create skillCards.js

---
+++ SkillCardAutoCarousel/Portfolio/data/skillCards.js
@@ -0,0 +1,94 @@
+import HTML from '../../../assets/Portfolio/skills-images/html.png';
+import CSS from '../../../assets/Portfolio/skills-images/css.png';
+import JavaScript from '../../../assets/Portfolio/skills-images/javascript.png';
+import ReactImg from '../../../assets/Portfolio/skills-images/react.png';
+import AWS from '../../../assets/Portfolio/skills-images/aws.png';
+import AWSCCP from '../../../assets/Portfolio/skills-images/aws-ccp.png';
+import GitHub from '../../../assets/Portfolio/skills-images/github.png';
+import Tailwind from '../../../assets/Portfolio/skills-images/tailwind.png';
+import SECPLUS from '../../../assets/Portfolio/skills-images/secplus.png';
+import JAVA from '../../../assets/Portfolio/skills-images/java.png';
+import PYTHON from '../../../assets/Portfolio/skills-images/python.png';
+import SQL from '../../../assets/Portfolio/skills-images/sql.png';
+import OPENAI from '../../../assets/Portfolio/skills-images/openai.png';
+
+export const cards = [
+    {
+        id: 1,
+        text: 'HTML',
+        src: HTML,
+        style: 'shadow-red-400'
+    },
+    {
+        id: 2,
+        text: 'CSS',
+        src: CSS,
+        style: 'shadow-blue-500'
+    },
+    {
+        id: 3,
+        text: 'JAVASCRIPT',
+        src: JavaScript,
+        style: 'shadow-yellow-600'
+    },
+    {
+        id: 4,
+        text: 'REACT',
+        src: ReactImg,
+        style: 'shadow-blue-300'
+    },
+    {
+        id: 5,
+        text: 'AWS',
+        src: AWS,
+        style: 'shadow-orange-400'
+    },
+    {
+        id: 6,
+        text: 'CLOUD PRACTITIONER',
+        src: AWSCCP,
+        style: 'shadow-gray-200'
+    },
+    {
+        id: 7,
+        text: 'GITHUB',
+        src: GitHub,
+        style: 'shadow-green-300'
+    },
+    {
+        id: 8,
+        text: 'TAILWIND CSS',
+        src: Tailwind,
+        style: 'shadow-teal-500'
+    },
+    {
+        id: 9,
+        text: 'COMPTIA SECURITY+',
+        src: SECPLUS,
+        style: 'shadow-red-500'
+    },
+    {
+        id: 10,
+        text: 'JAVA',
+        src: JAVA,
+        style: 'shadow-gray-100'
+    },
+    {
+        id: 11,
+        text: 'Python',
+        src: PYTHON,
+        style: 'shadow-yellow-500'
+    },
+    {
+        id: 12,
+        text: 'SQL',
+        src: SQL,
+        style: 'shadow-purple-300'
+    },
+    {
+        id: 13,
+        text: 'OPENAI GPT',
+        src: OPENAI,
+        style: 'shadow-green-500'
+    },
+  ]
